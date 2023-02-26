6a376ad071ffb3ef385159fe148b656949942b6f
Hana Ehab <hanamongi@hotmail.com>
2023-02-26 14:00:38 +0200

    remove duplicate rules

--- preprocessing/N3_preprocessing.py
+++ preprocessing/N3_preprocessing.py
@@ -12,7 +12,7 @@ def analyzeMetalStack(typesString):
     for index, type in enumerate(types):
         noOfOccurrences = int(typesOccurrences[index])
         for n in range(noOfOccurrences):
-            viaLayerName = "VIA" + str(counter)
+            viaLayerName = "VIA" + str(counter) if type.lower() != "ap" else "RV"
             viaTypeName = "VIA" + type.lower() if type.lower() != "ap" else "RV"
             numberOfVias[viaLayerName] = viaTypeName
             conventions[viaTypeName] = "V" + type.lower()
@@ -20,17 +20,18 @@ def analyzeMetalStack(typesString):
                 conventions2["AP"] = "RV"
             else:
                 conventions2[type.lower()] = "V" + type.lower()
-            counter += 1
+                counter += 1
 
 
 def n3Preprocessing(ruleDeckPath):
 
     encFile = open("files/enclosure_rules.txt", "w")
     dimFile = open("files/variable_rules.txt", "w")
+    typesWritten = []  #to avoid writting same type multiple times
 
     with open(ruleDeckPath, 'r') as file:  # Path of the rule deck
+
         for line in file:
-            # if (line.find(".EN.") > 0):
             if(re.search(r'^VIA\d*.EN.',line) or re.search(r'^M\d*.EN.',line) or re.search(r'^RV.EN.',line) or re.search(r'^AP.EN.',line) ):
                 regexp = re.compile(r'with the other 2\s*(long)?\s*sides(.*)')
                 if regexp.search(line):
@@ -50,17 +51,19 @@ def n3Preprocessing(ruleDeckPath):
 
                     encFile.write(afterPreprocessing)
 
-            # elif (((line.split('.')[0]).find("VIA") >= 0 or (line.split('.')[0]).find("RV") >= 0) and line.find(".W.1") > 0):
             elif(re.search(r'^VIA\d*.W.1',line) or re.search(r'^RV.W.1',line)):
                 ruleName = line.split()[0]
                 viaNumber = ruleName.split('.')[0]
                 if viaNumber in numberOfVias.keys():
+                    if(numberOfVias[viaNumber] in typesWritten):  #Find if the extracted type is already written
+                        continue
                     afterPreprocessing = ruleName.replace(
                         viaNumber, numberOfVias[viaNumber])
                     afterPreprocessing += line[line.find("@")+1:]
                 else:
                     afterPreprocessing = ruleName + line[line.find("@")+1:]
-
+                
+                typesWritten.append(numberOfVias[viaNumber])
                 dimFile.write(afterPreprocessing)
 
     encFile.close()
