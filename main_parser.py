28855beb6377cdef9377adedf3146d9487940559
hamami5w <hamza.amir.ext@siemens.com>
2023-03-10 19:13:51 +0200

    handling folders paths if not exist

--- main_parser.py
+++ main_parser.py
@@ -5,11 +5,35 @@ import parsers.spacing_test as spacing
 import preprocessing.N3_preprocessing as preprocessing
 import writing.writing_results as writing
 import sys
+import os
+
+# Create the output folders if any one not exist 
+def createIfNotExist():
+    outputFolder = "output_files"
+    spacingFolder = "output_files/spacings"
+    preprocessedFolder = "preprocessedFiles"
+
+    outputFolderCheck = os.path.exists(outputFolder)
+    spacingFolderCheck = os.path.exists(spacingFolder)
+    preprocessedFolderCheck = os.path.exists(preprocessedFolder)
+
+    if not outputFolderCheck:
+        os.makedirs(outputFolder)
+
+    if not spacingFolderCheck:
+        os.makedirs(spacingFolder)
+
+    if not preprocessedFolderCheck:
+        os.makedirs(preprocessedFolder)
+
 
 ruleDeckPath = sys.argv[1]
 metalStack = sys.argv[2]
 # The alternatives values of the enclosure rules (0 -> not printing them, 1 -> printing them)
 enclosureAltMode = sys.argv[3]
+
+# Create the output folders if any one not exist 
+createIfNotExist()
 
 # Generating a map with each via number and preprocess the rule deck (N3)
 preprocessing.analyzeMetalStack(metalStack)
